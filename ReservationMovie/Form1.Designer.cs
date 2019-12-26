56726268998cbca7e406d9fe883d67e0a22d8ff0
ulviibrahim <ulviia@code.edu.az>
2019-12-26 22:31:29 +0400

    second commit

---
+++ ReservationMovie/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace ReservationMovie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.PnlSeats = new System.Windows.Forms.Panel();
+            this.LblTitleSelected = new System.Windows.Forms.Label();
+            this.LblSelectedPl = new System.Windows.Forms.Label();
+            this.LblSum = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BtnBooking = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Zəhmət olmasa yerinizi seçin";
+            // 
+            // PnlSeats
+            // 
+            this.PnlSeats.BackColor = System.Drawing.Color.Transparent;
+            this.PnlSeats.Location = new System.Drawing.Point(360, 82);
+            this.PnlSeats.Name = "PnlSeats";
+            this.PnlSeats.Size = new System.Drawing.Size(300, 318);
+            this.PnlSeats.TabIndex = 1;
+            // 
+            // LblTitleSelected
+            // 
+            this.LblTitleSelected.AutoSize = true;
+            this.LblTitleSelected.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTitleSelected.ForeColor = System.Drawing.Color.Yellow;
+            this.LblTitleSelected.Location = new System.Drawing.Point(31, 112);
+            this.LblTitleSelected.Name = "LblTitleSelected";
+            this.LblTitleSelected.Size = new System.Drawing.Size(133, 20);
+            this.LblTitleSelected.TabIndex = 0;
+            this.LblTitleSelected.Text = "Seçilmiş Yerlər:";
+            // 
+            // LblSelectedPl
+            // 
+            this.LblSelectedPl.AutoSize = true;
+            this.LblSelectedPl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblSelectedPl.ForeColor = System.Drawing.Color.Yellow;
+            this.LblSelectedPl.Location = new System.Drawing.Point(31, 153);
+            this.LblSelectedPl.Name = "LblSelectedPl";
+            this.LblSelectedPl.Size = new System.Drawing.Size(0, 20);
+            this.LblSelectedPl.TabIndex = 2;
+            // 
+            // LblSum
+            // 
+            this.LblSum.AutoSize = true;
+            this.LblSum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblSum.ForeColor = System.Drawing.Color.Yellow;
+            this.LblSum.Location = new System.Drawing.Point(31, 248);
+            this.LblSum.Name = "LblSum";
+            this.LblSum.Size = new System.Drawing.Size(0, 20);
+            this.LblSum.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Yellow;
+            this.label3.Location = new System.Drawing.Point(31, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Toplam:";
+            // 
+            // BtnBooking
+            // 
+            this.BtnBooking.BackColor = System.Drawing.Color.Green;
+            this.BtnBooking.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.BtnBooking.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnBooking.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.BtnBooking.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnBooking.ForeColor = System.Drawing.Color.Cornsilk;
+            this.BtnBooking.Location = new System.Drawing.Point(51, 313);
+            this.BtnBooking.Name = "BtnBooking";
+            this.BtnBooking.Size = new System.Drawing.Size(138, 48);
+            this.BtnBooking.TabIndex = 5;
+            this.BtnBooking.Text = "Book";
+            this.BtnBooking.UseVisualStyleBackColor = false;
+            this.BtnBooking.Click += new System.EventHandler(this.BtnBooking_Click);
+            this.BtnBooking.MouseLeave += new System.EventHandler(this.BtnBooking_MouseLeave);
+            this.BtnBooking.MouseHover += new System.EventHandler(this.BtnBooking_MouseHover);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Lime;
+            this.button3.Location = new System.Drawing.Point(530, 25);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(27, 24);
+            this.button3.TabIndex = 11;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(461, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Selected";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.Location = new System.Drawing.Point(428, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(27, 24);
+            this.button1.TabIndex = 15;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(357, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Available";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(633, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(27, 24);
+            this.button2.TabIndex = 17;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(571, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 17);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Booked";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(897, 486);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.BtnBooking);
+            this.Controls.Add(this.LblSum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LblSelectedPl);
+            this.Controls.Add(this.LblTitleSelected);
+            this.Controls.Add(this.PnlSeats);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Booking";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel PnlSeats;
+        private System.Windows.Forms.Label LblTitleSelected;
+        private System.Windows.Forms.Label LblSelectedPl;
+        private System.Windows.Forms.Label LblSum;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button BtnBooking;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ReservationMovie/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+using System.Data.SqlClient;
+
+
+namespace ReservationMovie
+{
+    public partial class Form1 : Form
+
+    {
+        string connectionstring = @"Data Source = .\SQLEXPRESS; Initial Catalog = MoveSeats;Integrated Security=True";
+        SqlConnection conn;
+         private Hall Hall = new Hall();
+        private List<Button> SelectButton = new List<Button>();
+        private List<Button> Booked = new List<Button>();
+        Dictionary<string, Button> btns = new Dictionary<string, Button>();
+
+        private int Price = 5;
+        public Form1()
+        {
+            conn = new SqlConnection(connectionstring);
+
+             InitializeComponent();
+            AddtoHall();
+            GenrateBtn();
+        }
+        private void AddtoHall()
+        {
+            Hall.SeatCount = 30;
+            Hall.Column = 5;
+        }
+
+        private  void GenrateBtn()
+        {
+            int x = 5;
+            int y = 20;
+            for (int i = 0; i < Hall.SeatCount; i++)
+            {
+                Button btn = new Button();
+                btn.Size = new Size(40, 40);
+                btn.Text = (i + 1).ToString();
+                btns[btn.Text] = btn;
+                btn.Click += new EventHandler( this.selectBtn_click);
+                btn.Cursor = Cursors.Hand;
+                btn.BackColor = Color.Yellow;
+                conn.Open();
+                string dbSelectquery = $"select * from  Seats  where ChooseBool=1 and id='{btn.Text}'";
+                var Selectcommand = new SqlCommand(dbSelectquery, conn);
+                var Selectres = Selectcommand.ExecuteReader();
+
+                Selectres.Read();
+                //btns[Selectres.GetValue(0).ToString()].te
+
+                    if(Selectres.HasRows && btn.Text == Selectres.GetValue(0).ToString())
+                    {
+                        this.Booked.Add(btn);
+                        foreach (var item in Booked)
+                        {
+                            item.BackColor = Color.Red;
+                            item.Cursor = Cursors.No;
+                        }
+                    }
+                    else
+                    {
+                        btn.BackColor = Color.Yellow;
+                    }
+                
+                conn.Close();
+
+
+                btn.Location = new Point(x, y);
+                x += 60;
+                PnlSeats.Controls.Add(btn);
+                if ((i+1) % Hall.Column == 0)
+                {
+                    x = 5;
+                    y += 60;
+                }
+
+            }
+            
+        }
+      
+       
+        private void selectBtn_click(object sender,EventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn.BackColor==Color.Yellow)
+            {
+                btn.BackColor = Color.Lime;
+                
+                this.SelectButton.Add(btn);
+
+                
+
+            }
+            else if (btn.BackColor == Color.Lime)
+            {
+                btn.BackColor = Color.Yellow;
+                this.SelectButton.Remove(btn);
+            }
+            else
+            {
+                btn.Click -= new EventHandler( selectBtn_click);
+            }
+            SelectPlaces();
+            TotalPrice();
+        }
+        private void TotalPrice()
+        {
+            LblSum.Text = (this.SelectButton.Count*this.Price) + " azn";
+            if (LblSum.Text=="0 azn")
+            {
+                LblSum.Text = "";
+            }
+            
+        }
+
+        private void SelectPlaces()
+        {
+            List<string> Places = new List<string>();
+            foreach (var item in this.SelectButton)
+            {
+                Places.Add(item.Text);
+            }
+
+            LblSelectedPl.Text = string.Join(",", Places.ToArray());
+        }
+
+        private void BtnBooking_Click(object sender, EventArgs e)
+        {
+            if (SelectButton.Count<1)
+            {
+                MessageBox.Show("Evvelce Yerinizi secin");
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show("Seçilmiş yerləri təsdiqləməyə əminsiniz?", "Bilet satışı", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    foreach (Button btn in SelectButton)
+                    {
+                        conn.Open();
+                        string dbquery = $"update  Seats  set ChooseBool=1 where SelectedSeats= {btn.Text}";
+                        var command = new SqlCommand(dbquery, conn);
+                        var res = command.ExecuteNonQuery();
+                        conn.Close();
+
+                        btn.Cursor = Cursors.No;
+                        btn.BackColor = Color.Red;
+                    }
+
+                    SelectButton.Clear();
+                    LblSelectedPl.Text = "";
+                    LblSum.Text = "";
+                }
+            }
+        }
+
+        private void BtnBooking_MouseHover(object sender, EventArgs e)
+        {
+            BtnBooking.BackColor = Color.Red;
+        }
+       
+        private void BtnBooking_MouseLeave(object sender, EventArgs e)
+        {
+            BtnBooking.BackColor = Color.Green;
+        }
+    }
+}
